6ccb5096498201d1ebe712d3f0c4ddb546385e9d
Deepinder Singh Brar <deepindersinghdeep03@gmail.com>
2024-11-30 17:39:08 +0530

    created our own exception and called it by throwing error

---
+++ Exceptions_in_java/exceptionHandling.java
@@ -0,0 +1,22 @@
+
+
+public class myOwnException extends Exception{
+    public myOwnException(){ //our own created custom exception constructor
+        super("This is my own exception");
+    }
+}
+
+public class exceptionHandling {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try{
+        // int total = a/b;
+        if (b == 0){
+        throw new myOwnException();}; //custom exception being called
+        }catch(Exception e){
+            System.out.println("An unexpected exception occured ");
+            System.out.println("Error message: "+e);
+        }
+    }
+}
