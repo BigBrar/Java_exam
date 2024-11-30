d62ab250a4e5b01360cd226b13d1365899a97751
Deepinder Singh Brar <deepindersinghdeep03@gmail.com>
2024-11-30 15:01:50 +0530

    packages in java
    in this program we created our own "user defined" package. This was then imported in the main.java and called the method of that package.

---
+++ packages_in_java/main.java
@@ -0,0 +1,8 @@
+import myPackage.MyClass;
+
+public class main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.testingPackages();
+    }
+}
---
+++ packages_in_java/myPackage/MyClass.java
@@ -0,0 +1,8 @@
+package myPackage;
+import static java.lang.System.*;
+
+public class MyClass {
+    public void testingPackages(){
+        out.println("packages are working!!!");
+    }
+}
